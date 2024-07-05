5d9b2f599bb5d28d01f466aae71214ac7b877e91
devYuraKim <dev.yurakim@gmail.com>
2024-07-05 19:44:36 +0900

    solving NoUniqueBeanDefinitionException: @Primary (setting up default bean)

---
+++ example4/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,34 @@
+package com.example.config;
+
+import com.example.beans.Vehicle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    Vehicle vehicle1(){
+        Vehicle veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
+
+    @Bean
+    Vehicle vehicle2(){
+        Vehicle veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
+
+    @Bean
+    @Primary
+    Vehicle vehicle3(){
+        Vehicle veh = new Vehicle();
+        veh.setName("Ferrari");
+        return veh;
+    }
+
+
+}
