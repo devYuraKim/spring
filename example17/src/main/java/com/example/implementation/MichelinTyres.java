0c1fdfef77b0fbbf7499555c28bd29d6d49a2da9
devYuraKim <dev.yurakim@gmail.com>
2024-07-05 23:23:40 +0900

    AOP(Aspect Oriented Programming) practice set-up

---
+++ example17/src/main/java/com/example/implementation/MichelinTyres.java
@@ -0,0 +1,22 @@
+package com.example.implementation;
+
+
+import com.example.interfaces.Tyres;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
+public class MichelinTyres implements Tyres {
+
+    @Override
+    public String rotate(){
+        return "Vehicle moving with the help of "+this.getClass().getSimpleName();
+    }
+
+    @Override
+    public String stop() {
+        return "Vehicle stopped with the help of "+this.getClass().getSimpleName();
+    }
+
+}
