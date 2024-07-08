1dd9f2a585a69509c9dfba3688e2478d98bfebc9
devYuraKim <dev.yurakim@gmail.com>
2024-07-08 15:45:45 +0900

    example20: setting up web application

---
+++ example20/src/main/java/com/devyurakim/devschool/DevschoolApplication.java
@@ -0,0 +1,13 @@
+package com.devyurakim.devschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevschoolApplication.class, args);
+	}
+
+}
