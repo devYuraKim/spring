a3f8a6b58788259bc4bb1b5b49207ef6117995b2
devYuraKim <dev.yurakim@gmail.com>
2024-07-08 14:03:31 +0900

    Thymeleaf basic setting

---
+++ example19/src/main/java/com/devyurakim/devschool/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.devyurakim.devschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"", "/", "home"})
+    public String displayHomePage(Model model){
+        //Model: Interface acting as a container for data
+        model.addAttribute("username", "John Doe");
+        return "home.html";
+    }
+
+}
---
+++ example19/src/test/java/com/devyurakim/devschool/DevschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devyurakim.devschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
