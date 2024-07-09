b9ea539398f7956525d425db9920af0168f1078b
devYuraKim <dev.yurakim@gmail.com>
2024-07-09 21:19:08 +0900

    InMemoryAuthentication

---
+++ example29/src/main/java/com/devyurakim/devschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.devyurakim.devschool.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        // Permit All Requests inside the Web Application
+        http.csrf(csrf->csrf.disable()) //csrf disabling for POST, PUT requests
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/dashboard").authenticated()
+                .requestMatchers("/", "home").authenticated()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+
+        /**Deny All Requests inside the Web Application
+         * 코드를 삭제하지 않고 사용자의 접근을 막아야 할 때 사용
+         * http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build(); */
+
+    }
+
+    /**inMemoryAuthentication()
+     * UserDetails 객체 생성 후, InMemoryUserDetailsManager에 변수로 전달하면 끝*/
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(){
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+}
