b505bf65baa31061a2dd55e68601d8dba39a8280
devYuraKim <dev.yurakim@gmail.com>
2024-07-09 12:34:19 +0900

    Thymeleaf form field - Java object data binding with validation(@Valid, Errors)

--- example24/src/main/java/com/devyurakim/devschool/controller/ContactController.java
+++ example24/src/main/java/com/devyurakim/devschool/controller/ContactController.java
@@ -2,9 +2,13 @@ package com.devyurakim.devschool.controller;
 
 import com.devyurakim.devschool.model.Contact;
 import com.devyurakim.devschool.service.ContactService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,9 +28,13 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-        public String displayContactPage(){
-            return "contact.html";
-        }
+    public String displayContactPage(Model model){
+        /**user가 /contact로 naviage하면 새로운 Contact instance를 생성해서 model에 더하면
+         * (model이 automatic data binding을 가능하게 한다)
+         * 해당 object와 input form fields 간의 data binding을 시행한다*/
+         model.addAttribute("contact", new Contact());
+         return "contact.html";
+    }
 
         /**개별 Parameter를 처리하려면 번거로우므로 POJO 사용하는 코드로 변경*/
 //        @PostMapping("/saveMsg")
@@ -42,7 +50,12 @@ public class ContactController {
 //        }
 
         @PostMapping("/saveMsg")
-        public String saveMessage(Contact contact){
+        public String saveMessage(@Valid @ModelAttribute Contact contact, Errors errors){
+            if(errors.hasErrors()){
+                log.error("Contact form validation failed due to: "+errors.toString());
+                return "contact.html";
+                //redisplay contact.html view with error messages (@Valid가 가능하게 함)
+            }
             contactService.saveMessageDetails(contact);
             return "redirect:/contact";
         }
