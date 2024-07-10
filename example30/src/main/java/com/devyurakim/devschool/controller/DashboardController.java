ddee87ad6945992a801d4e2f39d3a01dfa3caa68
devYuraKim <dev.yurakim@gmail.com>
2024-07-10 19:44:14 +0900

    @ControllerAdvice+@ExceptionHandler

--- example30/src/main/java/com/devyurakim/devschool/controller/DashboardController.java
+++ example30/src/main/java/com/devyurakim/devschool/controller/DashboardController.java
@@ -14,8 +14,8 @@ public class DashboardController {
     public String displayDashboard(Model model, Authentication authentication) {
         model.addAttribute("username", authentication.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
-        throw new RuntimeException("Pain is killing me");
-        //return "dashboard.html";
+        //throw new RuntimeException("Pain is killing me");
+        return "dashboard.html";
     }
 
 }
