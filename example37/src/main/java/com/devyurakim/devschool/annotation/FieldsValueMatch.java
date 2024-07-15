9e268d04963cb592e307c9047e8fffe761a78cb5
devYuraKim <dev.yurakim@gmail.com>
2024-07-16 01:51:44 +0900

    custom validation annotation: annotation package

--- example37/src/main/java/com/devyurakim/devschool/annotation/FieldsValueMatch.java
+++ example37/src/main/java/com/devyurakim/devschool/annotation/FieldsValueMatch.java
@@ -14,13 +14,13 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FieldsValueMatch {
 
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
+    Class<?>[] groups() default {}; //그룹은 사용 시에 annotation 내부에서 설정
+    Class<? extends Payload>[] payload() default {}; //payload(메타데이터)는 사용 시에 annotation 내부에서 설정
 
-    String message() default "Fields values don't match!";
+    String message() default "필드 입력값이 일치하지 않음!";
 
+    /*두 개의 필드 입력값에 대해 비교를 실시할 것이므로 아래 코드가 필요함*/
     String field();
-
     String fieldMatch();
 
     @Target({ ElementType.TYPE })
--- example37/src/main/java/com/devyurakim/devschool/annotation/PasswordValidator.java
+++ example37/src/main/java/com/devyurakim/devschool/annotation/PasswordValidator.java
@@ -6,12 +6,17 @@ import jakarta.validation.Payload;
 
 import java.lang.annotation.*;
 
-@Documented
-@Constraint(validatedBy = PasswordStrengthValidator.class)
-@Target( { ElementType.METHOD, ElementType.FIELD })
-@Retention(RetentionPolicy.RUNTIME)
+@Documented //Javadoc 사용 시에 생성된 documentation에 해당 annotation 포함
+@Constraint(validatedBy = PasswordStrengthValidator.class) //custom logic이 포함된 class 명시
+@Target( { ElementType.METHOD, ElementType.FIELD }) //해당 annotation이 무엇을 대상으로 실행되는지
+@Retention(RetentionPolicy.RUNTIME) //해당 annotation이 어느 시점에 실행되는지
 public @interface PasswordValidator {
-    String message() default "Please choose a strong password";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
+    String message() default "Please choose a strong password"; //기본 에러 메시지
+    Class<?>[] groups() default {}; //annotation 그룹화 가능(기본, 심화 등...)
+    Class<? extends Payload>[] payload() default {}; //메타데이터 설정 가능
+
+    /* 이외에도 regexp(), flags()로 입력 형태 제어 가능하다
+    예시)
+    String regexp() default "[a-z]+";
+    Pattern.Flag[] flags() default { Pattern.Flag.CASE_INSENSITIVE }; */
 }
